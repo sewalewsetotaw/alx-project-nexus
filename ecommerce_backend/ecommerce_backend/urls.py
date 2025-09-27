7a89c0c277585c0d3830149642d273eb3a3025ea
sewalewsetotaw <sewalewsetotaw2023@gmail.com>
2025-09-28 00:26:18 +0300

    Fix Swagger static files issue on Render

--- ecommerce_backend/ecommerce_backend/urls.py
+++ ecommerce_backend/ecommerce_backend/urls.py
@@ -5,6 +5,8 @@ from rest_framework_simplejwt import views as jwt_views
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi 
+from django.conf import settings
+from django.conf.urls.static import static
 schema_view = get_schema_view(
    openapi.Info(
       title="E-Commerce App API",
@@ -35,3 +37,5 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
